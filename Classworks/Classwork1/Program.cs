00868135049736a9fd58c03279d364995d30b5f6
Андрей Миронов <avale1648@gmail.com>
2023-07-10 20:53:36 +0300

    Classwork 1

--- Classworks/Classwork1/Program.cs
+++ Classworks/Classwork1/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Текущая дата и время:\n" + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() );
+app.MapGet("/newYear", () => "До нового года осталось " + (new DateTime(2024, 1, 1) - DateTime.Now).Days + " дней.");
 app.Run();
