ed214979646978f7e47bce94a0e99743093d6e00
Андрей Миронов <avale1648@gmail.com>
2023-07-12 15:53:06 +0300

    Homework 1

--- Classworks/Classwork1/Program.cs
+++ Classworks/Classwork1/Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.MapGet("/", () => "Текущая дата и время:\n" + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() );
-app.MapGet("/newYear", () => "До нового года осталось " + (new DateTime(2024, 1, 1) - DateTime.Now).Days + " дней.");
+app.MapGet("/newYear", () => "До нового года осталось " + (new DateTime(DateTime.Now.Year + 1, 1, 1) - DateTime.Now).Days + " дней.");
 app.Run();
---
+++ Homeworks/Homework 1/Program.cs
@@ -0,0 +1,99 @@
+using Homework_1.Properties;
+using Microsoft.AspNetCore.Authentication;
+using System.Globalization;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.MapGet("/", () => "Домашняя работа 1\n\n/customs_duty?price=(float)[цена_посылки]&weight=(float)[вес_посылки] - стоимость налоговой пошлины;\n/current_date_time?lang=яз_ЯЗ - узнать сегодняшнюю дату в подробном формате");
+app.MapGet("/customs_duty", (float? price, float? weight) => GetDuty(price, weight));
+app.MapGet("/current_date_time", (string lang) => GetCurrentDate(lang));
+app.Run();
+string GetDuty(float? price, float? weight)
+{
+    try
+    {
+        return $"Налоговая пошлина\nНалоговая пошлина на вашу посылку составляет {(price = price < 200 && weight < 30 ? 0f : price / 100 * 15)} евро.";
+    }
+    catch(Exception ex)
+    {
+        return ex.Message;
+    }
+}
+string GetCurrentDate(string lang)
+{
+    string result = String.Empty;
+    try
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(lang);
+        result += DateTime.Now.Day + " ";
+        switch (DateTime.Now.Month)
+        {
+            case 1:
+                result += strings.january;
+                break;
+            case 2:
+                result += strings.february;
+                break;
+            case 3:
+                result += strings.march;
+                break;
+            case 4:
+                result += strings.april;
+                break;
+            case 5:
+                result += strings.may;
+                break;
+            case 6:
+                result += strings.june;
+                break;
+            case 7:
+                result += strings.july;
+                break;
+            case 8:
+                result += strings.august;
+                break;
+            case 9:
+                result += strings.september;
+                break;
+            case 10:
+                result += strings.october;
+                break;
+            case 11:
+                result += strings.november;
+                break;
+            case 12:
+                result += strings.december;
+                break;
+        }
+        result += " " + DateTime.Now.Year + ", ";
+        switch (DateTime.Now.DayOfWeek)
+        {
+            case DayOfWeek.Monday:
+                result += strings.monday;
+                break;
+            case DayOfWeek.Tuesday:
+                result += strings.tuesday;
+                break;
+            case DayOfWeek.Wednesday:
+                result += strings.wednesday;
+                break;
+            case DayOfWeek.Thursday:
+                result += strings.thursday;
+                break;
+            case DayOfWeek.Friday:
+                result += strings.friday;
+                break;
+            case DayOfWeek.Saturday:
+                result += strings.saturday;
+                break;
+            case DayOfWeek.Sunday:
+                result += strings.sunday;
+                break;
+        }
+        result += ", " + DateTime.Now.ToShortTimeString();
+    }
+    catch (CultureNotFoundException ex)
+    {
+        result = ex.Message;
+    }
+    return result;
+}
